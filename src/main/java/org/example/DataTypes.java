c761f9b30b2ba1b108707657297bd9e171526a7f
Sachinverma903 <verma1996sachin@gmail.com>
2023-07-19 19:35:19 +0530

    New commit

---
+++ src/main/java/org/example/DataTypes.java
@@ -0,0 +1,50 @@
+package org.example;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        byte num=5;
+         num = 10;
+         num = 20;
+        //System.out.println(num);
+
+        byte num2 = -55;
+        short num3 = 54;
+        short num4 = 8888;
+        int num5 = 85358;
+        int num7 = 5;
+        int multi = num5*num7;
+        int divide = num5/num7;
+        long num6 = 1;
+
+        float floatNum = 56.25f;
+        double doubleNum = 5647;
+       // System.out.println(doubleNum);
+
+        char character = 'k';
+       // System.out.println(character);
+
+       double num8 = num * floatNum;
+       //System.out.println(num8);
+       // System.out.println(num + floatNum);
+       // System.out.println(num+" "+num2);
+        String str = "amit ";           //always use "" with string
+        System.out.println(str);
+        String str2 = "verma";
+        System.out.println(str2);
+        System.out.println(str + str2 + num + num2);
+        System.out.println(num + num2 + str + str2);
+        System.out.println("my name is " + str + str2);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/org/example/UserInput.java
@@ -0,0 +1,23 @@
+package org.example;
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       int userInput1 = sc.nextInt();
+        System.out.println("the value you have entered is " + userInput1);
+        System.out.println("Please enter value 1");
+        int num1 = sc.nextInt();
+        System.out.println("Please enter value 2");
+        int num2 = sc.nextInt();
+        System.out.println("the sum of the given values is "+ (num1+num2));
+        System.out.println("please enter first name");
+        String firstName = sc.nextLine();
+        System.out.println("please enter last name");
+        String lastName = sc.nextLine();
+        System.out.println("your full name is " + firstName + " " + lastName);
+
+
+
+    }
+}
