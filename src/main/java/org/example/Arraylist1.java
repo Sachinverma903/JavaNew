6e3fb44bad5bab41df55b2fcd1621dd5e20fbc92
Sachinverma903 <verma1996sachin@gmail.com>
2023-08-19 19:51:36 +0530

    New commit

--- src/main/java/org/example/Arraylist1.java
+++ src/main/java/org/example/Arraylist1.java
@@ -1,16 +1,17 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Arraylist1 {
     public static void main(String[] args) {
     ArrayList<Integer> list1 = new ArrayList<>();
         list1.add(15);
-        list1.add(0,50);
-        list1.add(1, 70);
+        list1.add(50);
+        list1.add(70);
         list1.add(65);
         list1.remove(3);
-        System.out.println(list1.size());
+       System.out.println(list1.size());
         System.out.println(list1.get(0));
         for (int component:list1) {
             System.out.println(component);
@@ -18,5 +19,12 @@ public class Arraylist1 {
         for (int i = 0; i < list1.size(); i++) {
             System.out.println(list1.get(i));
         }
+        list1.clear();
+        list1.add(15);
+        list1.add(90);
+        list1.add(70);
+        list1.add(65);
+        Collections.sort(list1);
+        System.out.println(list1);
     }
 }
