3018babf2896f20061d2874c42fbd6734cbf5dd8
radhepriye <vermajyoti81266@gmail.com>
2023-08-20 18:30:13 +0530

    demo commit

---
+++ src/main/java/org/example/arraylist1.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class arraylist1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(0,10);
+        list1.add(1, 15);
+        list1.add(2, 20);
+        list1.add(3, 25);
+        list1.add(4,30);
+        //Collections.sort();
+        //list1.set(0, 35);
+        //list1.remove(2);
+       // list1.get(2);
+       // list1.clear();
+        //System.out.println(list1.get(2));
+        //System.out.println(list1.size());
+        System.out.println(list1);
+    }
+}
